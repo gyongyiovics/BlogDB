bbfc22136d7e128689f5deccf32160b4e5aaa7ff
gyongyiovics <gyongyimako@gmail.com>
2021-10-05 10:12:36 +0200

    added connections among tables - eg. user and role

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -4,6 +4,7 @@ import application.database.DBEngine;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
         DBEngine dbEngine = new DBEngine();
         //System.out.println(dbEngine.isConnected());
     }
